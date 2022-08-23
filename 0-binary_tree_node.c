44de798778328e3f444d6f229f7d6f8db8f15c2c
Abuka-Victor <abukavictoro@gmail.com>
2022-08-23 01:03:15 +0100

    Add binary tree print and header files

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - a function that creates a binary tree node
+ * @parent: A pointer to the parent node of the new node to create
+ * @value: value to put in the new node
+ *
+ * Return: a binary_tree_t node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+}
