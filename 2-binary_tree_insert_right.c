bfc25549256f8773c4d71c198de602a2362276f9
Abuka Victor <abukavictoro@gmail.com>
2022-08-23 14:29:00 -0700

    Task 2 fix

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,11 +11,6 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-
-/**
- * TODO: Refactor Left Pattern to right
- */
-
 	binary_tree_t *new_right;
 
 	if (!parent)
