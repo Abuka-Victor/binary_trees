fa18ee6d2600384210bd4af762844bbd873797d7
Peacebern <peace8282bern@gmail.com>
2022-08-24 21:43:54 +0100

    Update 9-binary_tree_height.c
    [fix] task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,52 +1,17 @@
 #include "binary_trees.h"
 
 /**
- * count_tree_left - counts the number of levels in the tree
- * @tree: the pointer to the node
- *
- * Return: number of levels
+ * binary_tree_size - that measures the size of a binary tree
+ * @tree: pointer node binary tree
+ * Return: measures the size of a binary tree
  */
-size_t count_tree_left(const binary_tree_t *tree)
+size_t binary_tree_size(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (-1);
-
-	return (1 + count_tree_left(tree->left));
-}
-
-/**
- * count_tree_right - counts the number of levels in the tree
- * @tree: the pointer to the node
- *
- * Return: number of levels
- */
-size_t count_tree_right(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (-1);
-	return (1 + count_tree_right(tree->right));
-}
-
-
-/**
- * binary_tree_height - A function that measures the height of a binary tree
- * @tree: a pointer to the root node of the tree to measure the height
- *
- * Return: if tree is null return 0, else return the height
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t number_one;
-	size_t number_two;
+	size_t size = 1;
 
 	if (!tree)
 		return (0);
-
-	number_two = count_tree_right(tree);
-	number_one = count_tree_left(tree);
-
-	if (number_one > number_two)
-		return (number_one);
-
-	return (number_two);
+	size += binary_tree_size(tree->left);
+	size += binary_tree_size(tree->right);
+	return (size);
 }
